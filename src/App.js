025da66115d794128440f76e3aedf3ce2a2582a8
Tut Tongchantra <tuttongchan@Tuts-MacBook-Air.local>
2021-02-28 01:17:27 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [temperatureValue, setTemperatureValue] = useState(10);
+  const [temperatureColor, setTemperatureColor] = useState('cold');
+
+  const increaseTemperature = () => {
+    if (temperatureValue === 30) return;
+
+    const newTemperature = temperatureValue + 1;
+
+    if (newTemperature >= 15) {
+      setTemperatureColor('hot');
+    }
+
+    setTemperatureValue(newTemperature);
+  };
+
+  const decreaseTemperature = () => {
+    if (temperatureValue === 0) return;
+
+    const newTemperature = temperatureValue - 1;
+
+    if (newTemperature <= 15) {
+      setTemperatureColor('cold');
+    }
+
+    setTemperatureValue(newTemperature);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app-container">
+      <div className="temperature-display-container">
+        <div className={`temperature-display ${temperatureColor}`}>
+          {temperatureValue}°C
+        </div>
+      </div>
+      <div className="button-container">
+        <button onClick={() => increaseTemperature()}>+</button>
+        <button onClick={() => decreaseTemperature()}>-</button>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <ul>
+        <li>money</li>
+        <li>hoes</li>
+        <li>power</li>
+        <li>principles</li>
+        <li>morals</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
